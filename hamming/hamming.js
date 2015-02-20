019509a7b7347c6fa4613c54289c0cfae2a6e259
Tom <thomas.zumsteg@gmail.com>
2015-02-19 16:18:07 -0800

    First iteration hamming

---
+++ hamming/hamming.js
@@ -0,0 +1,8 @@
+module.exports.compute = function( dnaA, dnaB ) {
+	var sum = 0;
+	if (dnaA.length !== dnaB.length) { throw 'DNA strands must be of equal length.'}
+	for( var i = 0; i <= dnaA.length; i++) {
+		if (dnaA.charAt(i) !== dnaB.charAt(i)) {sum++}
+	}
+	return sum;
+};
