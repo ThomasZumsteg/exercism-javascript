49fc0fd9afd71445704e1d26214e16e1082ac2db
Tom <thomas.zumsteg@gmail.com>
2015-02-22 12:49:15 -0800

    First iteration beer song

---
+++ beer-song/beer-song.js
@@ -0,0 +1,29 @@
+module.exports = {
+	verse: verse,
+	sing: sing,
+};
+
+function verse( verseNum ) {
+	/* Sings a verse of "bottles of beer on the wall" */
+	// Special cases for verses that include 0 (no more) and 1 (bottle[s])
+	var specialLines = {
+		0: "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
+		1: "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+		2: "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n",
+	};
+	// A normal verse
+	var normalLine = "%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottles of beer on the wall.\n";
+	var sprintf = require('util').format;
+	return verseNum in specialLines ? specialLines[verseNum] : sprintf(normalLine, verseNum, verseNum, verseNum - 1);
+};
+
+function sing( start, stop) {
+	/* Sings a set of verses of "Bottle of beer on the wall" */
+	// Default
+	stop || (stop = 0);
+	var song = [];
+	for(var verseNum = start; verseNum >= stop; verseNum--) {
+		song.push(verse(verseNum));
+	}
+	return song.join("\n");
+};
