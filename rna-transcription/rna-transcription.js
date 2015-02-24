fe9f6d38f0a6e1a146dc5599a3847d5b8f902de1
Tom <thomas.zumsteg@gmail.com>
2015-02-24 12:14:55 -0800

    First iteration rna-trascription

---
+++ rna-transcription/rna-transcription.js
@@ -0,0 +1,12 @@
+module.exports = function(dna) {
+	/* Tranlates dna to RNA */
+	var dnaToRna = makeHash("GCTA".split(''), "CGAU".split(''));
+	return dna.split('').map(function(e) { return dnaToRna[e] } ).join('');
+}
+
+function makeHash(keys, values) {
+	/* Makes a hash table from key and value arrays */
+	var hash = {};
+	keys.forEach(function(key, index) { hash[key] = values[index] });
+	return hash;
+}
