1f17a04d3254d2f3b0c2703636c934fed6f80644
Tom <thomas.zumsteg@gmail.com>
2015-03-28 16:50:27 -0700

    Actual iteration of linked-list

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,16 +1,87 @@
-/* Wrapper around array class */
-var LinkedList = function() { this.list = new Array() };
-LinkedList.prototype.push = function(val) { this.list.push(val) };
-LinkedList.prototype.pop = function() { return this.list.pop() };
-LinkedList.prototype.shift = function() { return this.list.shift() };
-LinkedList.prototype.unshift = function(val) { this.list.unshift(val) };
-LinkedList.prototype.count = function() { return this.list.length };
+var LinkedList = function() {
+	/* A linked list */
+	this.head = null;
+	this.tail = null;
+	this.length = 0;
+}
+
+LinkedList.prototype.push = function(val) {
+	/* Adds a value to the end of the linked list */
+	this.tail = new Element(val, null, this.tail);
+	if(this.length >= 1)
+		this.tail.prev.next = this.tail;
+	else
+		this.head = this.tail;
+	this.length++;
+};
+
+LinkedList.prototype.pop = function() {
+	/* Removes and returns a value from the end of the linked list */
+	if(this.length === 0)
+		return undefined;
+	this.length--;
+	var element = this.tail;
+	this.tail = element.prev;
+	if(this.length === 0)
+		this.head = this.tail;
+	element.delete();
+	return element.val;
+};
+
+LinkedList.prototype.unshift = function(val) {
+	/* Adds a value to the start of the linked list */
+	this.head = new Element(val, this.head, null);
+	if(this.length >= 1)
+		this.head.next.prev = this.head;
+	else
+		this.tail = this.head;
+	this.length++;
+};
+
+LinkedList.prototype.shift = function() {
+	/* Removes and returns a value from the start of the linked list */
+	if(this.length === 0)
+		return undefined;
+	this.length--;
+	var element = this.head;
+	this.head = element.next;
+	if(this.length === 0)
+		this.tail = this.head
+	element.delete();
+	return element.val;
+};
+
+LinkedList.prototype.count = function() {
+	/* The length of the linked list */
+	return this.length;
+};
 
 LinkedList.prototype.delete = function(val) {
-	/* Deletes the first occurance of a value */
-	var index = this.list.indexOf(val);
-	if( index > -1 )
-		this.list.splice(index, 1);
+	/* Removes the first element for a linked list with a certain value */ 
+	var element = this.head;
+	while(element && element.val !== val)
+		element = element.next;
+	if(element){
+		this.length--;
+		element.delete();
+	}
+};
+
+var Element = function(val, next, prev) {
+	/* Element of a linked list */
+	this.val = val;
+	this.next = next;
+	this.prev = prev;
+}
+
+Element.prototype.delete = function() {
+	/* Removes an element from the linked list */
+	if(this.prev)
+		this.prev.next = this.next;
+	if(this.next)
+		this.next.prev = this.prev;
+	this.next = null;
+	this.prev = null;
 };
 
 module.exports = LinkedList;
