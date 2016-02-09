a87683b5885d2e65d628792c9cbd99be7cdcdd20
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-02-09 09:45:58 -0800

    Updated leap to match tests

--- leap/leap.js
+++ leap/leap.js
@@ -1,4 +1,10 @@
-module.exports = function(year) { 
-	/* Test if year is leap year */
-	return year%4==0 && (year%100!=0 || year%400==0);
+var Year = function(year) {
+  this.year = year;
 }
+
+Year.prototype.isLeap = function() { 
+	/* Test if year is leap year */
+	return this.year%4==0 && (this.year%100!=0 || this.year%400==0);
+}
+
+module.exports = Year;
