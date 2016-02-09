f7c2faecd23f42cd206a668398235938a1104f8d
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-02-09 09:39:07 -0800

    Updated meetup to match new tests

--- meetup/meetup.js
+++ meetup/meetup.js
@@ -1,54 +1,49 @@
-var Meetup = function(month, year) {
+var weekDays = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(" ");
+
+var Meetup = function(year, month, day, nth) {
 	/* Finds a meetup date */
-	this.month = month;
-	this.year = year;
+  var intDay = weekDays.indexOf(day)
 
-	'sunteenth monteenth tuesteenth wednesteenth thursteenth friteenth saturteenth'.split(" ")
-		.forEach( function(func, day) { this[func] = find_day(day, 13, 19) }, this);
-
-	'firstSunday firstMonday firstTuesday firstWednesday firstThursday firstFriday firstSaturday'.split(' ')
-		.forEach( function(func, day) { this[func] = find_day(day, 1, 7) }, this);
-
-	'secondSunday secondMonday secondTuesday secondWednesday secondThursday secondFriday secondSaturday'.split(' ')
-		.forEach( function(func, day) { this[func] = find_day(day, 8, 14) }, this);
-
-	'thirdSunday thirdMonday thirdTuesday thirdWednesday thirdThursday thirdFriday thirdSaturday'.split(' ')
-		.forEach( function(func, day) { this[func] = find_day(day, 15, 21) }, this);
-
-	'fourthSunday fourthMonday fourthTuesday fourthWednesday fourthThursday fourthFriday fourthSaturday'.split(' ')
-		.forEach( function(func, day) { this[func] = find_day(day, 22, 28) }, this);
-
-	'fourthSunday fourthMonday fourthTuesday fourthWednesday fourthThursday fourthFriday fourthSaturday'.split(' ')
-		.forEach( function(func, day) { this[func] = find_day(day, 22, 28) }, this);
-
-	'lastSunday lastMonday lastTuesday lastWednesday lastThursday lastFriday lastSaturday'.split(' ')
-		.forEach( function(func, day) { this[func] = last(day) }, this);
+  switch (nth) {
+    case "teenth":
+      return find_day(year, month, intDay, 13, 19);
+    case "1st":
+      return find_day(year, month, intDay, 1, 7);
+    case "2nd":
+      return find_day(year, month, intDay, 8, 14);
+    case "3rd":
+      return find_day(year, month, intDay, 15, 21);
+    case "4th":
+      return find_day(year, month, intDay, 22, 28);
+    case "5th":
+      return find_day(year, month, intDay, 29, 31);
+    case "last":
+      return last(year, month, intDay);
+  }
 }
 
-function find_day(day, start, stop) {
+function find_day(year, month, day, start, stop) {
 	/* Finds the first occurance of a day of the week between two dates */
-	return function() {
-		for(var meetup = new Date(this.year, this.month, start); 
-			meetup.getDate() <= stop && meetup.getMonth() == this.month; 
-			meetup = meetup.addDays(1)) {
+  for(var meetup = new Date(year, month, start); 
+    meetup.getDate() <= stop && meetup.getMonth() == month; 
+    meetup = meetup.addDays(1)) {
 
-			if( meetup.getDay() == day )
-				return meetup;
-		}
-	};
+    if( meetup.getDay() == day )
+      return meetup;
+  }
+  throw "Date does not exist";
 };
 
-function last(day) {
+function last(year, month, day) {
 	/* Last occurance of a day in a month */
-	return function() {
-		for(var meetup = new Date(this.year, this.month + 1, 0); 
-			meetup.getMonth() == this.month; 
-			meetup = meetup.addDays(-1)) {
+  for(var meetup = new Date(year, month + 1, 0); 
+    meetup.getMonth() == month; 
+    meetup = meetup.addDays(-1)) {
 
-			if( meetup.getDay() == day )
-				return meetup;
-		}
-	};
+    if( meetup.getDay() == day )
+      return meetup;
+  }
+  throw "Date does not exist";
 };
 
 Date.prototype.addDays = function(days) {
