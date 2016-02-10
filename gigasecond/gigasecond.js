1052813464a7a807a7b8ebcf5a7b9e07073e3049
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-02-10 09:40:55 -0800

    Updated Gigasecond to use milliseconds instead of days to pass unit tests

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,13 +1,14 @@
-// days = days/hour * hours/second * seconds 
-var GIGASECOND =  (1/24) * (1 / 3600) * Math.pow(10, 9);
+// milliseconds 
+var GIGASECOND = Math.pow(10, 12);
 
 var Gigasecond = function(day) {
 	/* Adds a gigasecond (10^9 seconds) to a date */
-	this.later = new Date(day);
-	this.later.setDate(this.later.getDate() + GIGASECOND );
+	this.day = day;
 };
 
 // The date a gigasecond later
-Gigasecond.prototype.date = function() { return this.later };
+Gigasecond.prototype.date = function() { 
+  return new Date(this.day.getTime() + GIGASECOND);
+};
 
 module.exports = Gigasecond;
