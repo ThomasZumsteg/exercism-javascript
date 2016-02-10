4daec2a3a6cf88120b2006186bc7995f44fbc4b3
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-02-10 09:19:37 -0800

    Objectized hamming to pass unit tests

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,8 +1,12 @@
-module.exports.compute = function( dnaA, dnaB ) {
-	var sum = 0;
-	if (dnaA.length !== dnaB.length) { throw 'DNA strands must be of equal length.'}
-	for( var i = 0; i <= dnaA.length; i++) {
-		if (dnaA.charAt(i) !== dnaB.charAt(i)) {sum++}
-	}
-	return sum;
-};
+var Hamming = function() {};
+
+Hamming.prototype.compute = function( dnaA, dnaB ) {
+  var sum = 0;
+  if (dnaA.length !== dnaB.length) { throw 'DNA strands must be of equal length.'}
+  for( var i = 0; i <= dnaA.length; i++) {
+    if (dnaA.charAt(i) !== dnaB.charAt(i)) {sum++}
+  }
+  return sum;
+}
+
+module.exports = Hamming;
