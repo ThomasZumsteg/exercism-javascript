2acf5336df3d8d924f58948ca32f101864d9aa98
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-02-11 16:00:54 -0800

    objectized rna-transcription to pass unit tests

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,4 +1,6 @@
-module.exports = function(dna) {
+var DnaTranscriber = function() {};
+
+DnaTranscriber.prototype.toRna = function(dna) {
 	/* Tranlates dna to RNA */
 	var dnaToRna = makeHash("GCTA".split(''), "CGAU".split(''));
 	return dna.split('').map(function(e) { return dnaToRna[e] } ).join('');
@@ -10,3 +12,5 @@ function makeHash(keys, values) {
 	keys.forEach(function(key, index) { hash[key] = values[index] });
 	return hash;
 }
+
+module.exports = DnaTranscriber;
