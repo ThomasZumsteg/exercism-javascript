6b1211d39d126769b25492f977d51f1d4997ca8f
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-07-20 16:15:11 -0700

    First iteration rational-numbers
    Rounding error in Raise a real number to a positive rational number

--- rational-numbers/rational-numbers.js
+++ rational-numbers/rational-numbers.js
@@ -1,6 +1,6 @@
-var gcd = function(i, j) {
-    var a = Math.max(Math.abs(i), Math.abs(j));
-    var b = Math.min(Math.abs(i), Math.abs(j));
+var gcd = function(num, denom) {
+    var a = Math.max(Math.abs(num), Math.abs(denom));
+    var b = Math.min(Math.abs(num), Math.abs(denom));
     var tmp;
     while(b != 0) {
         tmp = b;
@@ -13,8 +13,10 @@ var gcd = function(i, j) {
 class Rational {
     constructor(num, denom) {
         var divisor = gcd(num, denom);
+        if(num < 0)
+            divisor *= -1;
         this.num = num / divisor;
-        this.denom = denom / divisor;
+        this.denom = (num == 0 && denom < 0 ? -1 : 1) * denom / divisor;
     }
 
     add(rational) {
@@ -31,6 +33,26 @@ class Rational {
 
     mul(rational) {
         return new Rational( this.num * rational.num, this.denom * rational.denom);
+    }
+
+    div(rational) {
+        return new Rational(this.num * rational.denom, this.denom * rational.num);
+    }
+
+    abs() {
+        return new Rational(Math.abs(this.num), Math.abs(this.denom));
+    }
+
+    exprational(exp) {
+        return new Rational(Math.pow(this.num, exp), Math.pow(this.denom, exp));
+    }
+
+    expreal(base) {
+        return Math.pow(base, this.num / this.denom);
+    }
+
+    reduce() {
+        return this;
     }
 }
 
--- rational-numbers/rational-numbers.spec.js
+++ rational-numbers/rational-numbers.spec.js
@@ -77,120 +77,120 @@ describe('Multiplication', function () {
 });
 
 describe('Division', function () {
-  xit('Divide two positive rational numbers', function () {
+  it('Divide two positive rational numbers', function () {
     var expected = new Rational(3, 4);
     expect(new Rational(1, 2).div(new Rational(2, 3))).toEqual(expected);
   });
 
-  xit('Divide a positive rational number by a negative rational number', function () {
+  it('Divide a positive rational number by a negative rational number', function () {
     var expected = new Rational(-3, 4);
     expect(new Rational(1, 2).div(new Rational(-2, 3))).toEqual(expected);
   });
 
-  xit('Divide two negative rational numbers', function () {
+  it('Divide two negative rational numbers', function () {
     var expected = new Rational(3, 4);
     expect(new Rational(-1, 2).div(new Rational(-2, 3))).toEqual(expected);
   });
 
-  xit('Divide a rational number by 1', function () {
+  it('Divide a rational number by 1', function () {
     var expected = new Rational(1, 2);
     expect(new Rational(1, 2).div(new Rational(1, 1))).toEqual(expected);
   });
 });
 
 describe('Absolute value', function () {
-  xit('Absolute value of a positive rational number', function () {
+  it('Absolute value of a positive rational number', function () {
     var expected = new Rational(1, 2);
     expect(new Rational(1, 2).abs()).toEqual(expected);
   });
 
-  xit('Absolute value of a negative rational number', function () {
+  it('Absolute value of a negative rational number', function () {
     var expected = new Rational(1, 2);
     expect(new Rational(-1, 2).abs()).toEqual(expected);
   });
 
-  xit('Absolute value of zero', function () {
+  it('Absolute value of zero', function () {
     var expected = new Rational(0, 1);
     expect(new Rational(0, 1).abs()).toEqual(expected);
   });
 });
 
 describe('Exponentiation of a rational number', function () {
-  xit('Raise a positive rational number to a positive integer power', function () {
+  it('Raise a positive rational number to a positive integer power', function () {
     var expected = new Rational(1, 8);
     expect(new Rational(1, 2).exprational(3)).toEqual(expected);
   });
 
-  xit('Raise a negative rational number to a positive integer power', function () {
+  it('Raise a negative rational number to a positive integer power', function () {
     var expected = new Rational(-1, 8);
     expect(new Rational(-1, 2).exprational(3)).toEqual(expected);
   });
 
-  xit('Raise zero to an integer power', function () {
+  it('Raise zero to an integer power', function () {
     var expected = new Rational(0, 1);
     expect(new Rational(0, 1).exprational(5)).toEqual(expected);
   });
 
-  xit('Raise one to an integer power', function () {
+  it('Raise one to an integer power', function () {
     var expected = new Rational(1, 1);
     expect(new Rational(1, 1).exprational(4)).toEqual(expected);
   });
 
-  xit('Raise a positive rational number to the power of zero', function () {
+  it('Raise a positive rational number to the power of zero', function () {
     var expected = new Rational(1, 1);
     expect(new Rational(1, 2).exprational(0)).toEqual(expected);
   });
 
-  xit('Raise a negative rational number to the power of zero', function () {
+  it('Raise a negative rational number to the power of zero', function () {
     var expected = new Rational(1, 1);
     expect(new Rational(-1, 2).exprational(0)).toEqual(expected);
   });
 });
 
 describe('Exponentiation of a real number to a rational number', function () {
-  xit('Raise a real number to a positive rational number', function () {
+  it('Raise a real number to a positive rational number', function () {
     var expected = 16.0;
     expect(new Rational(4, 3).expreal(8)).toEqual(expected);
   });
 
-  xit('Raise a real number to a negative rational number', function () {
+  it('Raise a real number to a negative rational number', function () {
     var expected = 0.3333333333333333;
     expect(new Rational(-1, 2).expreal(9)).toEqual(expected);
   });
 
-  xit('Raise a real number to a zero rational number', function () {
+  it('Raise a real number to a zero rational number', function () {
     var expected = 1.0;
     expect(new Rational(0, 1).expreal(2)).toEqual(expected);
   });
 });
 
 describe('Reduction to lowest terms', function () {
-  xit('Reduce a positive rational number to lowest terms', function () {
+  it('Reduce a positive rational number to lowest terms', function () {
     var expected = new Rational(1, 2);
     expect(new Rational(2, 4).reduce()).toEqual(expected);
   });
 
-  xit('Reduce a negative rational number to lowest terms', function () {
+  it('Reduce a negative rational number to lowest terms', function () {
     var expected = new Rational(-2, 3);
     expect(new Rational(-4, 6).reduce()).toEqual(expected);
   });
 
-  xit('Reduce a rational number with a negative denominator to lowest terms', function () {
+  it('Reduce a rational number with a negative denominator to lowest terms', function () {
     var expected = new Rational(-1, 3);
     expect(new Rational(3, -9).reduce()).toEqual(expected);
   });
 
-  xit('Reduce zero to lowest terms', function () {
+  it('Reduce zero to lowest terms', function () {
     var expected = new Rational(0, 1);
     expect(new Rational(0, 6).reduce()).toEqual(expected);
   });
 
-  xit('Reduce an integer to lowest terms', function () {
+  it('Reduce an integer to lowest terms', function () {
     var expected = new Rational(-2, 1);
     expect(new Rational(-14, 7).reduce()).toEqual(expected);
   });
 
-  xit('Reduce one to lowest terms', function () {
+  it('Reduce one to lowest terms', function () {
     var expected = new Rational(1, 1);
     expect(new Rational(13, 13).reduce()).toEqual(expected);
   });
