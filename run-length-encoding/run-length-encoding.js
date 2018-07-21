6b4c67586bec504659d72ad997f834c08eb5ce67
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-07-21 16:48:49 -0700

    Second iteration run-length-encoding

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -1,33 +1,10 @@
-function encode(str) {
-    if(str == '')
-        return str;
-    var count = 1;
-    var prev = str[0];
-    var result = '';
-    for(var i = 1; i <= str.length; i++) {
-        if(prev != str[i]) {
-            result += (count <= 1 ? "" : count.toString()) + prev;
-            prev = str[i];
-            count = 1;
-        } else {
-            count += 1;
-        }
-    }
-    return result;
-}
-
-function decode(str) {
-    var result = '';
-    var count = 0;
-    for(var i = 0; i < str.length; i++) {
-        if('0' <= str[i] && str[i] <= '9') {
-            count = parseInt(str[i]) + 10 * count;
-        } else {
-            result += str[i].repeat(count == 0 ? 1 : count);
-            count = 0;
-        }
-    }
-    return result;
-}
-
-module.exports =  {encode: encode, decode: decode};
+module.exports =  {
+    encode: function(text) {
+        return text.replace(/([\w])\1+/g, function(group, chr) {
+            return group.length + chr});
+    },
+    decode: function(text) {
+        return text.replace(/(\d+)(\w)/g, function(_, number, chr) {
+            return chr.repeat(number);
+        });
+    }};
