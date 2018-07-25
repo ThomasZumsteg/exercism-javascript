4d05ea699a75b0f04e49d8d72308b25681ab826c
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-07-24 20:29:36 -0700

    First iteration sublist

--- sublist/sublist.js
+++ sublist/sublist.js
@@ -4,25 +4,43 @@ class List {
     }
 
     compare(list) {
-        var result;
+        var swap = false;
+        var match = false;
+        var elementsMatch;
+
         var listA = this.elements;
         var listB = list.elements;
-        if(listA.length < listB.length) {
-            result = "SUBLIST";
-        } else if(listB.length < listA.length) {
-            result = "SUPERLIST";
-            listA = list.elements;
-            listB = this.elements;
-        } else {
-            result = "EQUAL";
+
+        if(listB.length > listA.length) {
+            swap = true;
+            var listA = list.elements;
+            var listB = this.elements;
         }
-        for(var i = 0; i < listB.length - listA.length; i++) {
-            // console.log("Next");
-            for(var j = 0; j < listA.length; j++) {
-                // console.log(listB[i + j] + " - " + listA[j]);
+
+        for(var i = 0; i <= listA.length - listB.length && !match; i++) {
+            var elementsMatch = true;
+            for(var j = 0; j < listB.length; j++) {
+                if(listA[i + j] != listB[j]) {
+                    elementsMatch = false;
+                    break;
+                }
+            }
+            if(elementsMatch) {
+                match = true;
+                break;
             }
         }
-        return result;
+
+        if(match || listB.length == 0) {
+            if(listB.length == listA.length) {
+                return "EQUAL";
+            } else if(swap) {
+                return "SUBLIST";
+            } else {
+                return "SUPERLIST";
+            }
+        }
+        return "UNEQUAL";
     }
 }
 
