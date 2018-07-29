eee7fff696a1bf5155f45ac5710316e2ef9c13e2
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-07-28 17:47:07 -0700

    Second iteration flatten-array

--- flatten-array/flatten-array.js
+++ flatten-array/flatten-array.js
@@ -1,16 +1,8 @@
 class Flattener {
     flatten(array) {
-        if(array == null) {
-            return [];
-        } else if(array.constructor != Array) {
-            return [array];
-        }
-
-        var result = [];
-        for(var i = 0; i < array.length; i++) {
-            result.push.apply(result, this.flatten(array[i]));
-        }
-        return result;
+        if(array == null) { return [] }
+        else if(array.constructor != Array) { return [array] }
+        return array.reduce((acc, elem) => acc.concat(this.flatten(elem)), []);
     }
 }
 
