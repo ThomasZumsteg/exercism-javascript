137fec62085c88b8286fdd21d1c4766a48776875
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-08-07 21:09:42 -0700

    Start on simple-linked-list

---
+++ simple-linked-list/simple-linked-list.js
@@ -0,0 +1,94 @@
+class List {
+    constructor(head) {
+        this.head = head;
+    }
+
+    push(element) {
+        var tail = this.head;
+        if(!tail) {
+            this.head = element;
+            return;
+        }
+        while(tail.next) {
+            tail = tail.next
+        }
+        tail.next = element;
+    }
+
+    unshift(element) {
+        element.next = this.head;
+        this.head = element;
+    }
+
+    shift() {
+        var retVal = this.head;
+        this.head = this.head.next;
+        retVal.next = undefined;
+        return retVal;
+    }
+
+    pop() {
+        var tail = this.head;
+        if(!tail) {
+            return;
+        } else if(!tail.next) {
+            this.head = undefined;
+            return;
+        } else if(!tail.next.next) {
+            tail.next = undefined;
+            return;
+        }
+        while(!tail.next.next) {
+            tail = tail.next;
+        }
+        var retVal = tail.next;
+        tail.next = undefined;
+        return retVal;
+    }
+
+    toArray() {
+        var array = [];
+        var head = this.head;
+        while(head) {
+            array.push(head.value)
+            head = head.next;
+        }
+        return array;
+    }
+
+    reverse() {
+        var vals = [];
+        while(this.head) {
+            vals.push(this.shift());
+        }
+        for(let val of vals) {
+            this.shift(val);
+        }
+    }
+}
+
+List.fromArray = function(elements) {
+    var ll = new List();
+    for(let val of elements) {
+        ll.push(new Element(val));
+    }
+    return ll;
+}
+
+class Element {
+    constructor(e, next) {
+        if(e == null) {
+            throw new Error("Requires a value");
+        }
+        this.value = e;
+        if(!(next instanceof Element || typeof next == "undefined")) {
+            throw new Error("Next value must be an Element");
+        }
+        this.next = next;
+    }
+}
+
+module.exports = {
+    List: List,
+    Element: Element,
+};
---
+++ simple-linked-list/simple-linked-list.spec.js
@@ -0,0 +1,161 @@
+var SimpleLinkedList  = require('./simple-linked-list');
+var List  = SimpleLinkedList.List;
+var Element = SimpleLinkedList.Element;
+
+describe('simple-linked-list', function () {
+  it('exports a List constructor', function () {
+    expect(List).toBeDefined();
+  });
+
+  it('exports a Element constructor', function () {
+    expect(Element).toBeDefined();
+  });
+
+  describe('Element', function () {
+    it('is a constructor', function () {
+      var el = new Element(1);
+      expect(el).toBeDefined();
+
+      /* eslint-disable new-cap */
+      expect(function () {
+        el = Element(1);
+      }).toThrow();
+      /* eslint-enable new-cap */
+    });
+
+    it('requires an argument', function () {
+      var el = new Element(1);
+      expect(el).toBeDefined();
+
+      expect(function () {
+        el = new Element();
+      }).toThrow();
+    });
+
+    it('has as a value', function () {
+      var el = new Element(1);
+      expect(el.value).toBe(1);
+    });
+
+    it('reference to next is undefined by default', function () {
+      var el = new Element(1);
+      expect(el.next).toBeUndefined();
+    });
+  });
+
+  it('accepts a reference to the next element', function () {
+    var elOne = new Element(1);
+    var elTwo = new Element(2, elOne);
+    expect(elTwo.next).toBe(elOne);
+  });
+
+  /* eslint-disable no-unused-vars */
+  it('requires an instance of Element as next element', function () {
+    expect(function () {
+      var el = new Element(1, false);
+    }).toThrow();
+    expect(function () {
+      var el = new Element(1, 'lorem ipsum');
+    }).toThrow();
+    expect(function () {
+      var el = new Element(1, []);
+    }).toThrow();
+    expect(function () {
+      var el = new Element(1, {});
+    }).toThrow();
+  });
+  /* eslint-enable no-unused-vars */
+
+  describe('List', function () {
+    it('is a constructor', function () {
+      var ll = new List();
+      expect(ll).toBeDefined();
+    });
+
+    it('allows you to append an element', function () {
+      var ll = new List();
+      var el = new Element(1);
+      ll.push(el);
+      expect(ll.head).toBe(el);
+
+      var ll2 = new List();
+      var elOne = new Element(1);
+      var elTwo = new Element(2);
+      ll2.push(elOne);
+      ll2.push(elTwo);
+      expect(ll2.head.next).toBe(elTwo);
+    });
+
+    it('allows you to prepend an element', function () {
+      var ll = new List();
+      var el = new Element(1);
+      ll.unshift(el);
+      expect(ll.head).toBe(el);
+
+      var ll2 = new List();
+      var elOne = new Element(1);
+      var elTwo = new Element(2);
+      ll2.unshift(elOne);
+      ll2.unshift(elTwo);
+      expect(ll2.head).toBe(elTwo);
+    });
+
+    it('allows you to remove the first element', function () {
+      var ll = new List();
+      ll.push(new Element(1));
+      ll.shift();
+      expect(ll.head).toBeUndefined();
+
+      ll.push(new Element(2));
+      ll.push(new Element(3));
+      ll.shift();
+      expect(ll.head.value).toBe(3);
+    });
+
+    it('allows you to remove the last element', function () {
+      var ll = new List();
+      ll.push(new Element(1));
+      ll.pop();
+      expect(ll.head).toBeUndefined();
+
+      ll.push(new Element(2));
+      ll.push(new Element(3));
+      ll.pop();
+      expect(ll.head.next).toBeUndefined();
+    });
+
+    it('allows you to convert an array to a List', function () {
+      var ll = List.fromArray([1, 2]);
+
+      expect(ll.head.value).toBe(1);
+      expect(ll.head.next.value).toBe(2);
+    });
+
+    it('allows you to convert a List into an array', function () {
+      var ll = new List();
+      ll.push(new Element(1));
+      ll.push(new Element(2));
+      var a = ll.toArray();
+
+      expect(a instanceof Array).toBe(true);
+      expect(a.length).toBe(2);
+      expect(a[0]).toBe(1);
+      expect(a[1]).toBe(2);
+
+      expect(ll.head.value).toBe(1);
+      expect(ll.head.next.value).toBe(2);
+    });
+
+    it('allows you to reverse a List', function () {
+      var ll = new List();
+      ll.push(new Element(1));
+      ll.push(new Element(2));
+      ll.push(new Element(3));
+      ll.reverse();
+
+      expect(ll.head.value).toBe(3);
+      expect(ll.head.next.value).toBe(2);
+      expect(ll.head.next.next.value).toBe(1);
+    });
+  });
+});
